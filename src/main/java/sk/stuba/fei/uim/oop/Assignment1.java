d562a08ba11b6be745fe1dfd1a7c61de3a3187c9
luciahrn <hrnciarikova.lucia@gmail.com>
2021-03-14 20:18:21 +0100

    zaciatok hry

--- src/main/java/sk/stuba/fei/uim/oop/Assignment1.java
+++ src/main/java/sk/stuba/fei/uim/oop/Assignment1.java
@@ -1,7 +1,54 @@
 package sk.stuba.fei.uim.oop;
+import java.io.*;
+import java.util.Scanner;
+
+
 
 public class Assignment1 {
+
+    public Assignment1(){
+        super();
+    }
+
+    public  void getPlayer() {
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+
+        System.out.println("Zadaj svoje meno");
+
+        String userName = myObj.nextLine();  // Read user input
+        System.out.println("Meno je : " + userName);  // Output user input
+
+    }
+
+    public  void nacitavajHracov() {
+        Scanner pomocna = new Scanner(System.in);
+        System.out.println("Pre zacatie hry a zaevidovanie hracov stlac 1:");
+        String  premenna = pomocna.nextLine();
+        int poradie=0;
+        while (premenna.equals("1")) {
+            this.getPlayer();
+            poradie=poradie+1;
+            System.out.println("\nPoradie hraca je: "+ poradie);
+            System.out.println("Ak si zapisal vsetkych hracov stlac 0 ak chces dalej zapisovat hracov stlac 1:");
+            premenna = pomocna.nextLine();
+
+        }
+        System.out.println("Hra zacina!");
+
+
+
+    }
+
+
+    public  void monopoly() {
+        this.nacitavajHracov();
+    }
+
+
     public static void main(String[] args) {
-        
+        Assignment1 monopoly= new Assignment1();
+        monopoly.monopoly();
     }
 }
+
+
---
+++ src/main/java/sk/stuba/fei/uim/oop/Player.java
@@ -0,0 +1,10 @@
+package sk.stuba.fei.uim.oop;
+
+public class Player {
+    private String playerName;
+
+    Player [] friends;
+    public Player(String name) {
+        this.playerName = name;
+    }
+}
